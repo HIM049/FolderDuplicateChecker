8934f1b4312ffab11600ded5ce9154db62807805
HIM049 <HIM_049@163.com>
2025-05-23 12:21:17 +0800

    perf: functionalization

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"path"
 )
@@ -10,46 +11,72 @@ func main() {
 	targetPath := "D:\\#work\\BS\\CustomLevels"
 	samplePath := "D:\\SteamLibrary\\steamapps\\common\\Beat Saber\\Levels\\【Fitness歌曲包】最新100首健身类型的高评分歌曲 更新至[2023-12-01]_WGzeyu"
 
-	var targetNames []string
-	var duplicatedPath []string
-	targetMap := make(map[string]struct{})
+	sampleMap := make(map[string]struct{})
 
-	sampleEntries, err := os.ReadDir(samplePath)
+	// Add sample to list
+	err := AddFolderToLMap(samplePath, sampleMap)
 	if err != nil {
-		panic(err)
+		log.Fatalln("failed to add folder:", err)
 	}
 
-	// 写入匹配样本
-	for _, entry := range sampleEntries {
-		targetNames = append(targetNames, entry.Name())
-	}
-
-	for _, name := range targetNames {
-		targetMap[name] = struct{}{}
-	}
-
-	entries, err := os.ReadDir(targetPath)
+	// check duplicated name
+	dupList, err := CheckDuplicated(targetPath, sampleMap)
 	if err != nil {
-		panic(err)
+		log.Fatalln("failed to check duplicate:", err)
 	}
 
-	for _, entry := range entries {
-		if _, found := targetMap[entry.Name()]; found {
-			fmt.Println("Found duplicated: ", entry.Name())
-			duplicatedPath = append(duplicatedPath, entry.Name())
-		}
-	}
-
-	fmt.Println("result: ", duplicatedPath)
-	fmt.Println("count: ", len(duplicatedPath))
+	log.Println("result: ", dupList)
+	log.Println("count: ", len(dupList))
 
 	var re string
 	fmt.Scan(&re)
 
 	if re != "" {
-		for _, name := range duplicatedPath {
-			//fmt.Println(path.Join(targetPath, name))
-			os.RemoveAll(path.Join(targetPath, name))
+		RemoveDuplicated(targetPath, dupList)
+	}
+}
+
+func CheckDuplicated(targetPath string, sampleMap map[string]struct{}) ([]string, error) {
+	var duplicated []string
+
+	entries, err := os.ReadDir(targetPath)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, entry := range entries {
+		if _, found := sampleMap[entry.Name()]; found {
+			duplicated = append(duplicated, entry.Name())
+		}
+	}
+
+	return duplicated, nil
+}
+
+func AddFolderToLMap(path string, sampleMap map[string]struct{}) error {
+	// Read Folder
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		return err
+	}
+
+	// Add to list
+	for _, entry := range entries {
+		sampleMap[entry.Name()] = struct{}{}
+	}
+
+	return nil
+}
+
+func RemoveDuplicated(targetPath string, duplist []string) {
+
+	for _, name := range duplist {
+		currentPath := path.Join(targetPath, name)
+
+		log.Println("Removing: ", currentPath)
+		err := os.RemoveAll(currentPath)
+		if err != nil {
+			log.Println("Failed to remove: ", currentPath)
 		}
 	}
 }
